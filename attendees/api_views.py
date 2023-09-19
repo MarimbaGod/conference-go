fb15f39e9be45bc78b77ccd8838daba3a5e99735
Kyle <bossertkt@gmail.com>
2023-09-19 12:45:41 -0700

    sept 19

--- attendees/api_views.py
+++ attendees/api_views.py
@@ -4,6 +4,16 @@ from .models import Attendee
 
 
 def api_list_attendees(request, conference_id):
+    response = []
+    attendees = Attendee.objects.all()
+    for attendee in attendees:
+        response.append(
+            {
+                "name": attendee.name,
+                "href": attendee.get_api_url(),
+            }
+        )
+    return JsonResponse({"conferenes": response})
     """
     Lists the attendees names and the link to the attendee
     for the specified conference id.
@@ -23,10 +33,22 @@ def api_list_attendees(request, conference_id):
         ]
     }
     """
-    return JsonResponse({})
 
 
 def api_show_attendee(request, id):
+    attendee = Attendee.objects.get(id=id)
+    return JsonResponse(
+        {
+            "email": attendee.email,
+            "name": attendee.name,
+            "company_name": attendee.company_name,
+            "created": attendee.created,
+            "conference": {
+                "name": attendee.conference.name,
+                "href": attendee.conference.get_api_url(),
+            },
+        }
+    )
     """
     Returns the details for the Attendee model specified
     by the id parameter.
@@ -46,4 +68,3 @@ def api_show_attendee(request, id):
         }
     }
     """
-    return JsonResponse({})
