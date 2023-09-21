6fae9ffbe5ac1ddc92dc969912bbb4935a9998d5
Kyle <bossertkt@gmail.com>
2023-09-20 19:55:36 -0700

    all good

--- attendees/api_views.py
+++ attendees/api_views.py
@@ -1,19 +1,61 @@
 from django.http import JsonResponse
-
+from common.json import ModelEncoder
 from .models import Attendee
+from django.views.decorators.http import require_http_methods
+from events.models import Conference, Location
+import json
 
 
+class AttendeeListEncoder(ModelEncoder):
+    model = Attendee
+    properties = ["name"]
+
+
+class AttendeeDetailEncoder(ModelEncoder):
+    model = Attendee
+    properties = [
+        "email",
+        "name",
+        "company_name",
+        "created",
+    ]
+
+
+@require_http_methods(["GET", "POST"])
 def api_list_attendees(request, conference_id):
-    response = []
-    attendees = Attendee.objects.all()
-    for attendee in attendees:
-        response.append(
-            {
-                "name": attendee.name,
-                "href": attendee.get_api_url(),
-            }
+    if request.method == "GET":
+        attendees = Attendee.objects.filter(conference=conference_id)
+        return JsonResponse(
+            {"attendees": attendees},
+            encoder=AttendeeListEncoder,
         )
-    return JsonResponse({"conferenes": response})
+    else:
+        content = json.loads(request.body)
+
+        try:
+            conference = Conference.objects.get(id=conference_id)
+            content["conference"] = conference
+        except Conference.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid conference id"},
+                status=400,
+            )
+        attendee = Attendee.objects.create(**content)
+        return JsonResponse(
+            attendee,
+            encoder=AttendeeDetailEncoder,
+            safe=False,
+        )
+    # response = []
+    # attendees = Attendee.objects.all()
+    # for attendee in attendees:
+    #     response.append(
+    #         {
+    #             "name": attendee.name,
+    #             "href": attendee.get_api_url(),
+    #         }
+    #     )
+    # return JsonResponse({"conferenes": response})
     """
     Lists the attendees names and the link to the attendee
     for the specified conference id.
@@ -35,20 +77,41 @@ def api_list_attendees(request, conference_id):
     """
 
 
+@require_http_methods(["DELETE", "GET", "PUT"])
 def api_show_attendee(request, id):
+    if request.method == "GET":
+        attendee = Attendee.objects.get(id=id)
+        return JsonResponse(
+            {"attendee": attendee},
+            encoder=AttendeeDetailEncoder,
+            safe=False,
+        )
+    elif request.method == "DELETE":
+        count, _ = Location.objects.filter(id=id).delete()
+        return JsonResponse({"deleted": count > 0})
+    else:
+        content = json.loads(request.body)
+    Attendee.objects.filter(id=id).update(**content)
     attendee = Attendee.objects.get(id=id)
     return JsonResponse(
-        {
-            "email": attendee.email,
-            "name": attendee.name,
-            "company_name": attendee.company_name,
-            "created": attendee.created,
-            "conference": {
-                "name": attendee.conference.name,
-                "href": attendee.conference.get_api_url(),
-            },
-        }
+        attendee,
+        encoder=AttendeeDetailEncoder,
+        safe=False,
     )
+
+    # attendee = Attendee.objects.get(id=id)
+    # return JsonResponse(
+    #     {
+    #         "email": attendee.email,
+    #         "name": attendee.name,
+    #         "company_name": attendee.company_name,
+    #         "created": attendee.created,
+    #         "conference": {
+    #             "name": attendee.conference.name,
+    #             "href": attendee.conference.get_api_url(),
+    #         },
+    #     }
+    # )
     """
     Returns the details for the Attendee model specified
     by the id parameter.
--- events/api_views.py
+++ events/api_views.py
@@ -1,8 +1,17 @@
 from django.http import JsonResponse
-
+from common.json import ModelEncoder
 from .models import Conference, Location
+from django.views.decorators.http import require_http_methods
+import json
+from .models import State
 
 
+class ConferenceListEncoder(ModelEncoder):
+    model = Conference
+    properties = ["name"]
+
+
+@require_http_methods(["GET", "POST"])
 def api_list_conferences(request):
     """
     Lists the conference names and the link to the conference.
@@ -22,16 +31,63 @@ def api_list_conferences(request):
         ]
     }
     """
-    response = []
-    conferences = Conference.objects.all()
-    for conference in conferences:
-        response.append(
-            {
-                "name": conference.name,
-                "href": conference.get_api_url(),
-            }
+    if request.method == "GET":
+        conferences = Conference.objects.all()
+        return JsonResponse(
+            {"conferences": conferences},
+            encoder=ConferenceListEncoder,
         )
-    return JsonResponse({"conferences": response})
+    else:
+        content = json.loads(request.body)
+
+        try:
+            location = Location.objects.get(id=content["location"])
+            content["location"] = location
+        except Location.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid location id"},
+                status=400,
+            )
+        conference = Conference.objects.create(**content)
+        return JsonResponse(
+            conference,
+            encoder=ConferenceDetailEncoder,
+            safe=False,
+        )
+    ###### commented out for the ConferenceListEncoder
+    # response = []
+    # conferences = Conference.objects.all()
+    # for conference in conferences:
+    #     response.append(
+    #         {
+    #             "name": conference.name,
+    #             "href": conference.get_api_url(),
+    #         }
+    #     )
+    # return JsonResponse({"conferences": response})
+
+
+class LocationListEncoder(ModelEncoder):
+    model = Location
+    properties = ["name"]
+
+
+class ConferenceDetailEncoder(ModelEncoder):
+    model = Conference
+    properties = [
+        "name",
+        "description",
+        "max_presentations",
+        "max_attendees",
+        "starts",
+        "ends",
+        "created",
+        "updated",
+        "location",
+    ]
+    encoders = {
+        "location": LocationListEncoder(),
+    }
 
 
 def api_show_conference(request, id):
@@ -61,34 +117,74 @@ def api_show_conference(request, id):
     """
     conference = Conference.objects.get(id=id)
     return JsonResponse(
-        {
-            "name": conference.name,
-            "starts": conference.starts,
-            "ends": conference.ends,
-            "description": conference.description,
-            "created": conference.created,
-            "updated": conference.updated,
-            "max_presentations": conference.max_presentations,
-            "max_attendees": conference.max_attendees,
-            "location": {
-                "name": conference.location.name,
-                "href": conference.location.get_api_url(),
-            },
-        }
+        conference,
+        encoder=ConferenceDetailEncoder,
+        safe=False,
     )
+    # return JsonResponse(
+    #     {
+    #         "name": conference.name,
+    #         "starts": conference.starts,
+    #         "ends": conference.ends,
+    #         "description": conference.description,
+    #         "created": conference.created,
+    #         "updated": conference.updated,
+    #         "max_presentations": conference.max_presentations,
+    #         "max_attendees": conference.max_attendees,
+    #         "location": {
+    #             "name": conference.location.name,
+    #             "href": conference.location.get_api_url(),
+    #         },
+    #     }
+    # )
 
 
+@require_http_methods(["GET", "POST"])
 def api_list_locations(request):
-    response = []
-    locations = Location.objects.all()
-    for location in locations:
-        response.append(
-            {
-                "name": location.name,
-                "href": location.get_api_url(),
-            }
+    if request.method == "GET":
+        locations = Location.objects.all()
+        return JsonResponse(
+            {"locations": locations},
+            encoder=LocationListEncoder,
         )
-    return JsonResponse({"locations": response})
+    else:
+        content = json.loads(request.body)
+
+        # Get the State object and put it in the content dict
+        state = State.objects.get(abbreviation=content["state"])
+        content["state"] = state
+
+        location = Location.objects.create(**content)
+        return JsonResponse(
+            location,
+            encoder=LocationDetailEncoder,
+            safe=False,
+        )
+
+    # else:
+    #     content = json.loads(request.body)
+
+    #     state = State.objects.get(abbreviation=content["state"])
+    #     content["state"] = state
+
+    #     location = Location.objects.create(**content)
+    #     return JsonResponse(
+    #         location,
+    #         encoder=LocationDetailEncoder,
+    #         safe=False,
+    #     )
+
+    ### OLD CODE ###
+    # response = []
+    # locations = Location.objects.all()
+    # for location in locations:
+    #     response.append(
+    #         {
+    #             "name": location.name,
+    #             "href": location.get_api_url(),
+    #         }
+    #     )
+    # return JsonResponse({"locations": response})
     """
     Lists the location names and the link to the location.
 
@@ -109,6 +205,21 @@ def api_list_locations(request):
     """
 
 
+class LocationDetailEncoder(ModelEncoder):
+    model = Location
+    properties = [
+        "name",
+        "city",
+        "room_count",
+        "created",
+        "updated",
+    ]
+
+    def get_extra_data(self, o):
+        return {"state": o.state.abbreviation}
+
+
+@require_http_methods(["DELETE", "GET", "PUT"])
 def api_show_location(request, id):
     """
     Returns the details for the Location model specified
@@ -126,16 +237,46 @@ def api_show_location(request, id):
         "state": the two-letter abbreviation for the state,
     }
     """
-    location = Location.objects.get(id=id)
-    return JsonResponse(
-        {
-            "name": location.name,
-            "city": location.city,
-            "room_count": location.room_count,
-            "created": location.created,
-            "updated": location.updated,
-            "state": {
-                "abbreviation": location.state.abbreviation,
-            },
-        }
-    )
+    if request.method == "GET":
+        location = Location.objects.get(id=id)
+        return JsonResponse(
+            location,
+            encoder=LocationDetailEncoder,
+            safe=False,
+        )
+    elif request.method == "DELETE":
+        count, _ = Location.objects.filter(id=id).delete()
+        return JsonResponse({"deleted": count > 0})
+    else:
+        content = json.loads(request.body)
+        try:
+            if "state" in content:
+                state = State.objects.get(abbreviation=content["state"])
+                content["state"] = state
+        except State.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid state abbreviation"},
+                status=400,
+            )
+        Location.objects.filter(id=id).update(**content)
+        # copied from get detail
+        location = Location.objects.get(id=id)
+        return JsonResponse(
+            location,
+            encoder=LocationDetailEncoder,
+            safe=False,
+        )
+
+    #########oldcode###### replaced by encoder
+    # return JsonResponse(
+    #     {
+    #         "name": location.name,
+    #         "city": location.city,
+    #         "room_count": location.room_count,
+    #         "created": location.created,
+    #         "updated": location.updated,
+    #         "state": {
+    #             "abbreviation": location.state.abbreviation,
+    #         },
+    #     }
+    # )
